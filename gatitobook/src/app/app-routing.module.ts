f2a7df3a3de8ee4e7ceeb1e97681e061a010c620
Luiz Eduardo de Oliveria Martins <luidigastech@gmail.com>
2022-04-24 22:57:13 -0300

    Criando pagina de Login

--- gatitobook/src/app/app-routing.module.ts
+++ gatitobook/src/app/app-routing.module.ts
@@ -9,8 +9,8 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: ()=> import('./home/home.module').then( (m) =>m.HomeModule )
-  }
+    loadChildren: ()=> import('./home/home.module').then( (m) =>m.HomeModule ),
+  },
 
 ];
 
